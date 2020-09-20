2818e7d41d1b813ba0a015ff1344e52ee44ac684
Randeep <rsrsingh420@gmail.com>
2020-09-20 08:03:03 -0700

    settings edited

--- app/src/main/java/com/bcabuddies/blogair/settings/SettingsAccount.java
+++ app/src/main/java/com/bcabuddies/blogair/settings/SettingsAccount.java
@@ -7,21 +7,44 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.bcabuddies.blogair.R;
+import com.bcabuddies.blogair.utils.Constants;
+import com.bcabuddies.blogair.utils.PreferenceManager;
+import com.bumptech.glide.Glide;
 import com.google.android.material.textfield.TextInputEditText;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsAccount extends AppCompatActivity {
 
-    TextInputEditText userBioEt;
+
+    TextInputEditText userBioEt,fullNameEt;
+    CircleImageView thumbImage;
+    ImageView addImageIcon;
+    Button saveBtn;
+    PreferenceManager preferenceManager;
+
+    String thumbImageUrl, fullName;
     int lastSpecialRequestsCursorPosition;
-    String specialRequests="";
+    String specialRequests = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_account);
 
-        userBioEt=findViewById(R.id.settingsscc_biotv);
+        preferenceManager = new PreferenceManager(this);
+        userBioEt = findViewById(R.id.settingsscc_biotv);
+        thumbImage = findViewById(R.id.settingsscc_thumbimage);
+        addImageIcon = findViewById(R.id.settingsscc_addphotoicon);
+        saveBtn = findViewById(R.id.settingsscc_savebtn);
+        fullNameEt=findViewById(R.id.settingsscc_nametv);
+
+        thumbImageUrl = preferenceManager.getString(Constants.KEY_THUMB_IMAGE);
+        fullName = preferenceManager.getString(Constants.KEY_FUll_NAME);
 
         //limit user bio input lines
         userBioEt.addTextChangedListener(new TextWatcher() {
@@ -42,13 +65,16 @@ public class SettingsAccount extends AppCompatActivity {
                 if (userBioEt.getLineCount() > 5) {
                     userBioEt.setText(specialRequests);
                     userBioEt.setSelection(lastSpecialRequestsCursorPosition);
-                }
-                else
+                } else
                     specialRequests = userBioEt.getText().toString();
 
                 userBioEt.addTextChangedListener(this);
             }
         });
+
+        Glide.with(this).load(thumbImageUrl).into(thumbImage);
+        fullNameEt.setText(fullName);
+
 
 
     }
--- app/src/main/java/com/bcabuddies/blogair/settings/SettingsMain.java
+++ app/src/main/java/com/bcabuddies/blogair/settings/SettingsMain.java
@@ -6,39 +6,58 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.bcabuddies.blogair.R;
+import com.bcabuddies.blogair.utils.Constants;
+import com.bcabuddies.blogair.utils.PreferenceManager;
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsMain extends AppCompatActivity {
 
-    ConstraintLayout accountSettingLayout ,changePasswordLayout, blockedUsersLayout;
+    ConstraintLayout accountSettingLayout, changePasswordLayout, blockedUsersLayout;
+    CircleImageView thumbImageView;
+    String fullName,thumbImageUrl;
+    PreferenceManager preferenceManager;
+    TextView fullNameTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_main);
 
-        accountSettingLayout=findViewById(R.id.settings_accountlaoyut);
-        changePasswordLayout=findViewById(R.id.settings_changepasslayout);
-        blockedUsersLayout=findViewById(R.id.settings_blockeduserlayout);
+        preferenceManager = new PreferenceManager(this);
+        accountSettingLayout = findViewById(R.id.settings_accountlaoyut);
+        changePasswordLayout = findViewById(R.id.settings_changepasslayout);
+        blockedUsersLayout = findViewById(R.id.settings_blockeduserlayout);
+        thumbImageView = findViewById(R.id.settings_thumbicon);
+        fullNameTv=findViewById(R.id.settings_fullnameTv);
 
+        thumbImageUrl = preferenceManager.getString(Constants.KEY_THUMB_IMAGE);
+        fullName = preferenceManager.getString(Constants.KEY_FUll_NAME);
+
+        Glide.with(this).load(thumbImageUrl).into(thumbImageView);
+        fullNameTv.setText(fullName);
+
+        //click listeners
         accountSettingLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SettingsMain.this,SettingsAccount.class));
+                startActivity(new Intent(SettingsMain.this, SettingsAccount.class));
             }
         });
-
         changePasswordLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SettingsMain.this,SettingsChangePassword.class));
+                startActivity(new Intent(SettingsMain.this, SettingsChangePassword.class));
             }
         });
-
         blockedUsersLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SettingsMain.this,SettingsBlockedUsers.class));
+                startActivity(new Intent(SettingsMain.this, SettingsBlockedUsers.class));
             }
         });
     }
